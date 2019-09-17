af35a1c8403edb7af0001ba5e223375460ad3832
Elvis Ximenes <elvisximenes@Elviss-MacBook-Pro-2.local>
2019-09-17 18:59:06 +0900

    implement form

---
+++ resources/views/layouts/_messages.blade.php
@@ -0,0 +1,6 @@
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong>
+        {{ session('success') }}
+    </div>
+@endif
--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,7 +19,7 @@
                         @csrf
                         <div class="form-group">
                                 <label for="question-title">Title</label>
-                                <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}">
+                                <input type="text" name="title" value="{{ old('title') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}">
                                 @if ($errors->has('title'))
                                     <div class="invalid-feedback">
                                         <strong>{{ $errors->first('title') }}</strong>
@@ -28,7 +28,7 @@
                         </div>
                         <div class="form-group">
                                 <label for="question-body">Title</label>
-                                <textarea  name="body" id="question-body" rows="10" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}"></textarea>
+                                <textarea  name="body" id="question-body" rows="10" value="{{ old('body') }}"  class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}"></textarea>
                                 @if ($errors->has('body'))
                                     <div class="invalid-feedback">
                                         <strong>{{ $errors->first('body') }}</strong>
